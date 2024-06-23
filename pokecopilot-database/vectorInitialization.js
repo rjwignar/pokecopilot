b73343109709ce5bae4afd89af7f2f2bd3397e0b
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-06-23 09:58:30 -0400

    added addCollectionContentVectorField() from https://github.com/AzureCosmosDB/Azure-OpenAI-Node.js-Developer-Guide/tree/main/Labs/vector_search#vectorize-and-store-the-embeddings-in-each-document, to be reviewed modified to match database

--- pokecopilot-database/vectorInitialization.js
+++ pokecopilot-database/vectorInitialization.js
@@ -28,4 +28,62 @@ async function generateEmbeddings(text) {
     return embeddings.data[0].embedding;
 }
 
+async function addCollectionContentVectorField(db, collectionName) {
+    const collection = db.collection(collectionName); 
+    const docs = await collection.find({}).toArray();
+    const bulkOperations = [];
+    console.log(`Generating content vectors for ${docs.length} documents in ${collectionName} collection`);
+    for (let i=0; i<docs.length; i++) {
+        const doc = docs[i];
+        // do not include contentVector field in the content to be embedded
+        if ('contentVector' in doc) {
+            delete doc['contentVector'];
+        }
+        const content = JSON.stringify(doc);
+        const contentVector = await generateEmbeddings(content);
+        bulkOperations.push({
+            updateOne: {
+                filter: { '_id': doc['_id'] },
+                update: { '$set': { 'contentVector': contentVector } },
+                upsert: true
+            }
+        });
+        //output progress every 25 documents
+        if ((i+1) % 25 === 0 || i === docs.length-1) {          
+            console.log(`Generated ${i+1} content vectors of ${docs.length} in the ${collectionName} collection`);
+        }
+    }
+    if (bulkOperations.length > 0) {
+        console.log(`Persisting the generated content vectors in the ${collectionName} collection using bulkWrite upserts`);
+        await collection.bulkWrite(bulkOperations);
+        console.log(`Finished persisting the content vectors to the ${collectionName} collection`);
+    }
+
+    //check to see if the vector index already exists on the collection
+    console.log(`Checking if vector index exists in the ${collectionName} collection`)
+    const vectorIndexExists = await collection.indexExists('VectorSearchIndex');
+    if (!vectorIndexExists) {
+        await db.command({
+            "createIndexes": collectionName,
+            "indexes": [
+            {
+                "name": "VectorSearchIndex",
+                "key": {
+                "contentVector": "cosmosSearch"
+                },
+                "cosmosSearchOptions": {                  
+                "kind": "vector-ivf",
+                "numLists": 1,
+                "similarity": "COS",
+                "dimensions": 1536
+                }
+            }
+            ]
+        });
+        console.log(`Created vector index on contentVector field on ${collectionName} collection`);
+    }
+    else {
+        console.log(`Vector index already exists on contentVector field in the ${collectionName} collection`);
+    }
+}
 main().catch(console.error);
