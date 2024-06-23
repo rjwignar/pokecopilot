36f333de3797ea1fa7f78daf175217b8384b4e33
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-06-23 09:52:58 -0400

    imported AzureOpenAI Client and Azure Key Credential classes, setup AzureOpenAI Client, changed db name to pokemon

--- pokecopilot-database/vectorInitialization.js
+++ pokecopilot-database/vectorInitialization.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
-
+// Import AzureOpenAI Client and Azure Key Credential Classes
+const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
 // set up the MongoDB client
 const dbClient = new MongoClient(process.env.MONGODB_URI);
-
+// set up the Azure OpenAI client 
+const embeddingsDeploymentName = "embeddings";
+const completionsDeploymentName = "completions";
+const aoaiClient = new OpenAIClient(process.env.AOAI_ENDPOINT, new AzureKeyCredential(process.env.AOAI_KEY));
 async function main() {
     try {
         await dbClient.connect();
         console.log('Connected to MongoDB');
-        const db = dbClient.db('cosmic_works');
-
-        
+        const db = dbClient.db('pokemon');
     } catch (err) {
         console.error(err);
     } finally {
