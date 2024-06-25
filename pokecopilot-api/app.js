b416f821d0c9d58c4d551f007038de2988d4e1ce
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-06-25 18:13:20 -0400

    drafted GET /api/pokemon and GET /api/pokemon/:id routes

--- pokecopilot-api/app.js
+++ pokecopilot-api/app.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors')
 const swagger = require('./swagger');
 // const CosmicWorksAIAgent = require('./cosmic_works/cosmic_works_ai_agent');
 const PokecopilotAIAgent = require('./pokecopilot/pokecopilot_ai_agent');
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(process.env.AZURE_COSMOSDB_CONNECTION_STRING);
 const app = express();
 app.use(express.json());
 app.use(cors()); // enable all CORS requests
@@ -12,6 +15,63 @@ app.use(cors()); // enable all CORS requests
 // This is for demonstration only and should be hydrated by storing these
 // values in a database rather than in-memory.
 let agentInstancesMap = new Map();
+
+
+// Cosmos DB Collection Routes
+// GET All Pokemon
+
+/* Get all Pokemon endpoint. */
+/**
+ * @openapi
+ * /api/pokemon:
+ *   get:
+ *     description: Get all Pokemon endpoint
+ *     responses:
+ *       200:
+ *         description: Returns all pokemon in database
+ */
+app.get('/api/pokemon', async (req, res) => {
+    try {
+        const db = client.db("pokecopilot");
+        const collection = db.collection('pokemon');
+        const pokemon = await collection.find().toArray();
+        res.json(pokemon);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error fetching Pokémon' });
+    }
+});
+
+// GET One Pokemon by pokedex id
+
+/* Get one Pokemon endpoint. */
+/**
+ * @openapi
+ * /api/pokemon:
+ *   get:
+ *     description: Get one Pokemon by pokedex id
+ *     responses:
+ *       200:
+ *         description: Returns one pokemon in database
+ */
+app.get('/api/pokemon/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        const db = client.db("pokecopilot");
+        const collection = db.collection('pokemon');
+        const pokemon = await collection.findOne({ _id: id });
+        if (!pokemon) {
+            res.status(404).json({ message: 'Pokémon not found' });
+        } else {
+            res.json(pokemon);
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error fetching Pokémon' });
+    }
+});
+
+
 
 /* Health probe endpoint. */
 /**
@@ -75,10 +135,9 @@ swagger(app)
 var port = (() => {
     const { argv } = require('node:process');
     var port = 4242; // default
-    if (argv){
+    if (argv) {
         argv.forEach((v, i) => {
-            if (v && (v.toLowerCase().startsWith('port=')))
-            {
+            if (v && (v.toLowerCase().startsWith('port='))) {
                 port = v.substring(5);
             }
         });
