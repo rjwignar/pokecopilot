29265e0e811526ab2b3795b8bd75f7dfb6a0fe99
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-06-26 15:06:18 -0400

    ran format script on src folder

--- pokecopilot-client/src/components/ClearChatButton/ClearChatButton.tsx
+++ pokecopilot-client/src/components/ClearChatButton/ClearChatButton.tsx
@@ -4,17 +4,17 @@ import { Button } from "@fluentui/react-components";
 import styles from "./ClearChatButton.module.css";
 
 interface Props {
-    className?: string;
-    onClick: () => void;
-    disabled?: boolean;
+  className?: string;
+  onClick: () => void;
+  disabled?: boolean;
 }
 
 export const ClearChatButton = ({ className, disabled, onClick }: Props) => {
-    return (
-        <div className={`${styles.container} ${className ?? ""}`}>
-            <Button icon={<Delete24Regular />} disabled={disabled} onClick={onClick}>
-                {"Clear chat"}
-            </Button>
-        </div>
-    );
+  return (
+    <div className={`${styles.container} ${className ?? ""}`}>
+      <Button icon={<Delete24Regular />} disabled={disabled} onClick={onClick}>
+        {"Clear chat"}
+      </Button>
+    </div>
+  );
 };
